d35681fc13867a2b06dece5b88edb99d09877ef0
Kasia Drobniewicz <kasia.drobniewicz@gmail.com>
2018-01-10 14:58:36 +0100

    Add StudentModel class

---
+++ StudentModel.java
@@ -0,0 +1,11 @@
+public class StudentModel extends UserModel {
+    //ClassModel myClass;
+    //WalletModel wallet;
+    
+    
+    public StudentModel(Integer ID, String firstName, String lastName, String email, String password) {
+        super(ID, firstName, lastName, email, password);
+        // TODO
+    } 
+    
+}
