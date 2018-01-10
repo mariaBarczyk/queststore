45881d56a58ecda2c4a7ce56043a88e55d2081ea
Kasia Drobniewicz <kasia.drobniewicz@gmail.com>
2018-01-10 14:58:02 +0100

    Add AdminModel class

---
+++ AdminModel.java
@@ -0,0 +1,7 @@
+public class AdminModel extends UserModel {
+    
+    public AdminModel(Integer ID, String firstName, String lastName, String email, String password) {
+        super(ID, firstName, lastName, email, password);
+    } 
+    
+}
