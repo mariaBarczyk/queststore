c9ecb9fa7104fa3984763c6e26f96c5e47165a0a
Justyna Fornalik <jfornalik@gmail.com>
2018-01-10 23:31:24 +0100

    Add WalletModel class

---
+++ WalletModel.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+
+public class WalletModel {
+    private int totalCoolcoins;
+    private int balance;
+    private ArrayList<ArtifactModel> artifactCollection;
+
+    public WalletModel(){
+        this.totalCoolcoins = 0; //potrzebne potem do leveli
+        this.balance = 0;
+        this.artifactCollection = new ArrayList<ArtifactModel>();
+    }
+
+
+}
