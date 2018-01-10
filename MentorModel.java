604f4ef39189e1196814681f4adf9497e7fbdd22
Kasia Drobniewicz <kasia.drobniewicz@gmail.com>
2018-01-10 14:59:09 +0100

    Add MentorModel class

---
+++ MentorModel.java
@@ -0,0 +1,10 @@
+public class MentorModel extends UserModel {
+    //ClassModel classAssigned;
+
+    public MentorModel(Integer ID, String firstName, String lastName, String email, String password) {
+        super(ID, firstName, lastName, email, password);
+        // TODO
+    } 
+    
+    
+}
