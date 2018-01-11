79e3f19d669c3ab33bf6200221ef54e2d0e9439b
mariaBarczyk <barczyk.maria92@gmail.com>
2018-01-11 11:40:56 +0100

    Implement creating and adding to list new class in ClassDao

--- ClassDao.java
+++ ClassDao.java
@@ -4,4 +4,11 @@ import java.util.List;
 public class ClassDao {
 
     List<ClassModel> classList = new ArrayList<>();
+
+    public ClassModel createClass(String name) {
+        return new ClassModel(name);
+    }
+    public void addClass(ClassModel newClass) {
+        classList.add(newClass);
+    }
 }
