43dafea767b3962d24b91e88e425587c138471df
mariaBarczyk <barczyk.maria92@gmail.com>
2018-01-11 17:24:17 +0100

    Changes in creating mentor method in Admin

---
+++ AdminView.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+import java.util.Arrays;
+
+public class AdminView {
+
+    public String[] getMentorData() {
+        Scanner scanner = new Scanner(System.in);
+        String[] mentorData  = new String[4];
+        String[] atributes = {"name", "surname", "email", "password"};
+        for (String atribute: atributes) {
+            System.out.println(atribute);
+            System.out.println("Enter mentor " + atribute + ": ");
+            String atributeValue = scanner.nextLine();
+            mentorData[Arrays.asList(atributes).indexOf(atribute)] = atributeValue;
+            scanner.close();
+        }
+        return mentorData;
+    }
+    public String getUserInput(Strin text) {
+        Scanner scanner = new Scanner(System.in);
+        String data = scanner.nextLine();
+        scanner.close();
+        return data;
+    }
+    public void displayAdminMenu(){
+        System.out.println("1. Create mentor" +
+                           "2. Create class" + 
+                           "3. Edit mentor" +
+                           "4. Display mentor data");
+    }
+}
+
+
