74219e4e60ab0285bec3684f1068100c39263c7e
mariaBarczyk <barczyk.maria92@gmail.com>
2018-01-11 17:26:58 +0100

    Add method from interface in StudentDao

--- StudentDao.java
+++ StudentDao.java
@@ -1,17 +1,15 @@
 import java.util.ArrayList;
 import java.util.List;
 
-public class StudentDao {
+public class StudentDao implements StudentDaoInterface {
     private List<StudentModel> studentsList;
 
     public StudentDao() {
         this.studentsList = new ArrayList<StudentModel>();
     }
-
     public List<StudentModel> getStudentsList() {
         return this.studentsList;
     }
-
     public void addStudent() {
         StudentModel student1 = new StudentModel(1, "Kasia", "Drobna", "kasia@gmail.com", "kitka");
         StudentModel student2 = new StudentModel(2, "Marysia", "Barszczyk", "maria@gmail.com", "kawa");
@@ -19,6 +17,14 @@ public class StudentDao {
         this.studentsList.add(student1);
         this.studentsList.add(student2);
         this.studentsList.add(student3);
+    }
+    public void getStudentsByClass(){
+
+    }
+    public void getStudent() {
+
+    }
+    public void createStudent() {
     }
 
     
