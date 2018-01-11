050ae47b773b76d96f83eac7f48d2af6d90a1f10
Kasia Drobniewicz <kasia.drobniewicz@gmail.com>
2018-01-11 23:20:43 +0100

    Solve merge issues

--- MentorDaoInterface.java
+++ MentorDaoInterface.java
@@ -1,17 +1,8 @@
 import java.util.List;
 
 public interface MentorDaoInterface {
-<<<<<<< HEAD
-
-    public List getAllMentors();
-
-    public MentorModel createMentor();
-
-    public MentorModel getMentor();
-=======
     public List<MentorModel> getMentorsList(); 
     public void createMentor(String[] mentorData); 
     public void addMentor(MentorModel newMentor); 
     public MentorModel getMentor(int index);
->>>>>>> admin
 }
--- StudentDao.java
+++ StudentDao.java
@@ -11,9 +11,11 @@ public class StudentDao implements StudentDaoInterface {
         return this.studentsList;
     }
     public void addStudent() {
-        StudentModel student1 = new StudentModel("Kasia", "Drobna", "kasia@gmail.com", "kitka", "Student");
-        StudentModel student2 = new StudentModel("Marysia", "Barszczyk", "maria@gmail.com", "kawa", "Student");
-        StudentModel student3 = new StudentModel("Justyna", "Fornalik", "justi@gmail.com", "herbata", "Student");
+        WalletModel newWallet = new WalletModel();
+        ClassModel newClass = new ClassModel();
+        StudentModel student1 = new StudentModel("Kasia", "Drobna", "kasia@gmail.com", "kitka", "Student", newClass, newWallet);
+        StudentModel student2 = new StudentModel("Marysia", "Barszczyk", "maria@gmail.com", "kawa", "Student", newClass, newWallet);
+        StudentModel student3 = new StudentModel("Justyna", "Fornalik", "justi@gmail.com", "herbata", "Student", newClass, newWallet);
         this.studentsList.add(student1);
         this.studentsList.add(student2);
         this.studentsList.add(student3);
