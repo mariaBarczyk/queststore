f00e8712a9f59f29ed72deecd33527dd3536e08d
mariaBarczyk <barczyk.maria92@gmail.com>
2018-01-12 01:53:09 +0100

    Add mentor menu in mentorView

--- MentorView.java
+++ MentorView.java
@@ -1,36 +1,34 @@
 import java.util.Scanner;
 
 public class MentorView {
-        public void displayMentorMenu(){
-            System.out.println("1 - Create student's account.\n"
-                    + "2 - Add a new quest.\n"
-                    + "3 - Add new artifact to store.\n"
-                    + "4 - Update existing quest.\n"
-                    + "5 - Update existing artifact\n"
-                    + "6 - Mark student's achieved quests\n"
-                    + "7 - Mark student's bought artifacts\n"
-                    + "8 - See student's wallet");
-        }
-
-
-        public int getInput() {
-            int option = 0;
-            boolean optionNotSelected = true;
-            while (optionNotSelected) {
-                System.out.println("Select a number: ");
-                Scanner scanner = new Scanner(System.in);
-                try {
-                    option = scanner.nextInt();
-                    if (option>0 && option<9) {
-                        optionNotSelected = false;}
-                    else{
-                        System.out.println("Invalid input");
-                    }
-                } catch (java.util.InputMismatchException e) {
-                    System.out.println("Invalid input!");
+    public static void displayMentorMenu(){
+        System.out.println("1 - Create student's account.\n"
+                + "2 - Add a new quest.\n"
+                + "3 - Add new artifact to store.\n"
+                + "4 - Update existing quest.\n"
+                + "5 - Update existing artifact\n"
+                + "6 - Mark student's achieved quests\n"
+                + "7 - Mark student's bought artifacts\n"
+                + "8 - See student's wallet");
+    }
+    public int getInput() {
+        int option = 0;
+        boolean optionNotSelected = true;
+        while (optionNotSelected) {
+            System.out.println("Select a number: ");
+            Scanner scanner = new Scanner(System.in);
+            try {
+                option = scanner.nextInt();
+                if (option>0 && option<9) {
+                    optionNotSelected = false;}
+                else{
+                    System.out.println("Invalid input");
                 }
+            } catch (java.util.InputMismatchException e) {
+                System.out.println("Invalid input!");
             }
-            return option;
+        }
+        return option;
         }
 
     }
