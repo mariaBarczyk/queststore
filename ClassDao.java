bf3e393f45bfc763e1df4cdd9fcb2209ca0769b9
mariaBarczyk <barczyk.maria92@gmail.com>
2018-01-12 01:52:25 +0100

    Add hardcode in classDao

--- ClassDao.java
+++ ClassDao.java
@@ -6,9 +6,18 @@ public class ClassDao {
     List<ClassModel> classList = new ArrayList<>();
 
     public ClassModel createClass(String name) {
-        return new ClassModel(name);
+        ClassModel newClass = new ClassModel(name);
+        addClass(newClass);
+        return newClass;
     }
     public void addClass(ClassModel newClass) {
         classList.add(newClass);
+    }
+    public void importClassData() {
+        ClassModel class1 = new ClassModel("Group1");
+        ClassModel class2 = new ClassModel("Group2");
+        addClass(class1);
+        addClass(class2);
+
     }
 }
