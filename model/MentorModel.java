ee2ec41f40ba4a48931e2f094fe323751aa0a017
mariaBarczyk <barczyk.maria92@gmail.com>
2018-01-19 23:01:31 +0100

    Add group parameter and gettter in MentorModel

--- model/MentorModel.java
+++ model/MentorModel.java
@@ -2,8 +2,15 @@ package queststore.model;
 
 public class MentorModel extends UserModel {
 
-    public MentorModel(String firstName, String lastName, String email, String password) {
+    private GroupModel group;
+
+    public MentorModel(String firstName, String lastName, String email, String password, GroupModel group) {
         super(firstName, lastName, email, password, "Mentor");
-        
+        this.group = group  ; 
     }
+
+    public GroupModel getGroup() {
+        return this.group;
+    }
+
 }
