b350f6058c3725b243c4001a6d196ca19fda3d09
mariaBarczyk <barczyk.maria92@gmail.com>
2018-01-19 22:59:41 +0100

    Add group collection field and getter in AdminModel

--- model/AdminModel.java
+++ model/AdminModel.java
@@ -1,10 +1,18 @@
 package queststore.model;
 
 import queststore.model.MentorModel;
+import java.util.List;
+import java.util.ArrayList;
 
 public class AdminModel extends UserModel {
+
+    private static final List<GroupModel> allGroups = new ArrayList<>();
     
     public AdminModel(String firstName, String lastName, String email, String password, String status) {
         super(firstName, lastName, email, password, status);
     }   
+    
+    public static List<GroupModel> getGroupList() {
+        return allGroups;
+    }
 }
