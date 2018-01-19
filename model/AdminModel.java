32ae5940e119fee98d5466da020ed3acac427a77
mariaBarczyk <barczyk.maria92@gmail.com>
2018-01-19 18:56:37 +0100

    Create AdminModel class with constructor

---
+++ model/AdminModel.java
@@ -0,0 +1,8 @@
+package queststore.model;
+
+public class AdminModel extends UserModel {
+    
+    public AdminModel(String firstName, String lastName, String email, String password, String status) {
+        super(firstName, lastName, email, password, status);
+    }   
+}
