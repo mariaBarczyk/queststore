1bf68a686e8587c6590672bb80782f3a6c3cb75b
mariaBarczyk <barczyk.maria92@gmail.com>
2018-01-19 17:21:25 +0100

    Create UserModel class with constructor

---
+++ model/UserModel.java
@@ -0,0 +1,44 @@
+package queststore.model;
+
+public class UserModel {
+
+    private Integer ID;
+    private static Integer LastID = 0; 
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+    private String status;
+
+
+    public UserModel(String firstName, String lastName, String email, String password, String status) {
+            this.ID = LastID;
+            LastID += 1;
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.email = email;
+            this.password = password;
+            this.status = status;    
+    } 
+    public String getFirstName() {
+        return this.firstName;
+    }
+    public String getLastName() {
+        return this.lastName;
+    }
+    public String getEmail() {
+        return this.email;
+    }
+    public String getPassword() {
+        return this.password;
+    }
+    public String getStatus() {
+        return this.status;
+    }
+    public String toString(){
+        return this.firstName + " " + this.lastName + " " + this.email + " " + this.password + " " + this.status;
+    }
+    public Integer getID() {
+        return this.ID;
+    }
+}
