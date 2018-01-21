3b5fa0f6ebaeabbe6dda05fe629820fab9b4f0a2
mariaBarczyk <barczyk.maria92@gmail.com>
2018-01-21 23:28:38 +0100

    Add getters and setters in ItemModel

--- model/ItemModel.java
+++ model/ItemModel.java
@@ -1,25 +1,40 @@
 package queststore.model;
 
+import java.util.List;
+import java.util.ArrayList;
+
 public abstract class ItemModel {
     
     String type;
     String name;
     String description;
     int value;
+    private static List<ItemModel> itemCollection = new ArrayList<>(); 
     
     public ItemModel(String type, String name, String description, int value) {
         this.type = type;
         this.name = name;
         this.description = description;
         this.value = value;
+        itemCollection.add(this);
+
     }
     public String getName() {
         return this.name;
+    }
+    public String getType() {
+        return this.type;
     }
     public String getDescription() {
         return this.name;
     }
     public String getValue() {
         return this.name;
+    }
+    public void setValue(int newValue) {
+        this.value = newValue;
+    }
+    public static List<ItemModel> getItemCollection() {
+        return itemCollection;
     }
 }
