a3cbc37a88cd50f2e65c292e1db152516cc08da7
mariaBarczyk <barczyk.maria92@gmail.com>
2018-01-21 21:39:33 +0100

    Implement createQuest method in MentorController

--- controller/MentorController.java
+++ controller/MentorController.java
@@ -3,7 +3,9 @@ package queststore.controller;
 import queststore.view.MentorView;
 import queststore.model.StudentModel;
 import queststore.model.WalletModel;
+import queststore.controller.InputController;
 import queststore.model.GroupModel;
+import queststore.model.QuestModel;
 
 public class MentorController {
 
@@ -26,5 +28,12 @@ public class MentorController {
         WalletModel wallet = new WalletModel();
         StudentModel newStudent = new StudentModel(studentName, studentLastName, studentEmail, studentPassword, group, wallet);
     }
-
+    public QuestModel createQuest() {
+        String questName = inputController.getStringInput("Enter quest name: ");
+        String questDescription = inputController.getStringInput("Enter quest description: ");
+        int questValue = inputController.getIntInput("Enter quest value: ");
+        QuestModel newQuest = new QuestModel("Quest", questName, questDescription, questValue);
+        System.out.println(newQuest.getName());
+        return newQuest;
+    }
 }
