a566f160aa29db7b8d27318cd2007a26402f6c2e
mariaBarczyk <barczyk.maria92@gmail.com>
2018-01-21 21:26:08 +0100

    Add questCollection field in QuestModel class

--- model/QuestModel.java
+++ model/QuestModel.java
@@ -1,6 +1,12 @@
+package queststore.model;
+
+import java.util.List;
+import java.util.ArrayList;
+
 public class QuestModel extends ItemModel { 
     
         boolean used;
+        private static List<QuestModel> questCollection = new ArrayList<>(); 
     
         public QuestModel(char type, String name, String description, int value) {
             super(type, name, description, value);
