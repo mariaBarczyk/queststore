94a2510d354e8a602138d9d757ad243ff8f79937
mariaBarczyk <barczyk.maria92@gmail.com>
2018-01-22 11:25:45 +0100

    Add artifactCollection field in WalletModel

--- model/WalletModel.java
+++ model/WalletModel.java
@@ -1,23 +1,29 @@
 package queststore.model;
 
-// import java.util.ArrayList;
-// import java.util.List;
+import java.util.ArrayList;
+import java.util.List;
 
 public class WalletModel {
 
     private int totalCoolcoins;
     private int balance;
-    // private List<ArtifactModel> artifactCollection;
+    private List<ArtifactModel> artifactCollection;
 
     public WalletModel() {
         this.totalCoolcoins = 0;
         this.balance = 0;
-        // this.artifactCollection = new ArrayList<ArtifactModel>();
+        this.artifactCollection = new ArrayList<ArtifactModel>();
     }
     public int getBalance() {
         return balance;
     }
     public int getTotalCoolcoins() {
         return totalCoolcoins;
+    }
+    public void setTotalCoolcoins(int value) {
+        this.totalCoolcoins += value;
+    }
+    public void setBalance(int value) {
+        this.balance += value;
     }
 }
