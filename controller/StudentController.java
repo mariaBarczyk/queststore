825617ff9145a67efe7b9615b7993afca7ad5303
mariaBarczyk <barczyk.maria92@gmail.com>
2018-01-22 20:34:35 +0100

    Add buyArtifact method and method to controll menu options

--- controller/StudentController.java
+++ controller/StudentController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import queststore.controller.InputController;
 import queststore.model.ItemModel;
+import queststore.model.StudentModel;
 import queststore.view.StudentView;
 
 public class StudentController {
@@ -15,11 +16,44 @@ public class StudentController {
         view = new StudentView();
         inputController = new InputController();
     }
+
+    public void controlMenuOptions() {
+        boolean exit = false;
+        while (!exit) {
+            view.displayStudentMenu();
+            int userChoice = inputController.getIntInput("SELECT AN OPTION: ");
+            switch (userChoice) {
+                case 1:
+                    //view.displayWallet();
+                    break;
+                case 2:
+                    //buyArtifact(student);
+                    break;
+                case 3:
+                    //Buy artifact together with teammates;
+                    break;
+                case 4:
+                    //4 - See your level of experience\n";
+                    break;                                                         
+                case 9:
+                    exit = true;
+                    break;
+                default:
+                    System.out.println("Wrong number!");
+            }
+        }
+    }
+      
     private void displayAvailableArtifacts() {
          List<ItemModel>  artifactCollection = ItemModel.getCollectionByType("Artifact");
          view.displayCollectionOfItem(artifactCollection);
     }
-    public void buyArtifact() {
-
+    public void buyArtifact(StudentModel student) {
+        displayAvailableArtifacts();
+        String artifactName = inputController.getStringInput("Enter artifact name to buy: ");
+        List<ItemModel> availableArtifacts = ItemModel.getCollectionByType("Artifact");
+        for (ItemModel artifact: availableArtifacts)
+            if(artifact.getName().equals(artifactName))
+                student.setValuesInWallet(artifact.getValue());
     }
 }
