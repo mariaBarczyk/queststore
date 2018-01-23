409cbf992a892acc407d6372b688205ce85ba7ef
Justyna Fornalik <jfornalik@gmail.com>
2018-01-23 14:34:21 +0100

    Add insertNewMentor method

--- src/dao/MentorDB.java
+++ src/dao/MentorDB.java
@@ -1,5 +1,20 @@
 package dao;
 
-public class MentorDB{
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+
+public class MentorDB {
+
+    public void insertNewMentor(String mentorName, String mentorLastName, String mentorEmail, String mentorPassword, String mentorGroup) {
+        SqlDB dataBase = new SqlDB();
+        String table = 'Mentor';
+        String columns = "(first_name, last_name, email, password, id_status, id_group)";
+        String values = "('" + mentorName + "', '" + mentorLastName + "', '" + mentorEmail + "','" + mentorPassword + "', " + 2 + ", " + 1 + ");";
+        dataBase.insertDataIntoTable(table, columns, values);
+    }
 
 }
+
