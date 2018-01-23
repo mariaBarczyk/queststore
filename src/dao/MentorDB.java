9dfd3bb7ab3d82203a387cee4fefdb020199af2e
Justyna Fornalik <jfornalik@gmail.com>
2018-01-23 22:58:49 +0100

    Update methods in MentorDB class

--- src/dao/MentorDB.java
+++ src/dao/MentorDB.java
@@ -1,26 +1,21 @@
 package dao;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.Statement;
-
 
 public class MentorDB {
 
     public void insertNewMentor(String mentorName, String mentorLastName, String mentorEmail, String mentorPassword, String mentorGroup) {
-        SqlDB dataBase = new SqlDB();
+        QueststoreDao dao = new Queststore();
         String table = "Mentor";
         String columns = "(first_name, last_name, email, password, id_status, id_group)";
         String values = "'" + mentorName + "', '" + mentorLastName + "', '" + mentorEmail + "','" + mentorPassword + "', " + 2 + ", " + 1 + "";
-        dataBase.insertDataIntoTable(table, columns, values);
+        dao.insertDataIntoTable(table, columns, values);
     }
 
     public void updateMentorData(int mentorsId, String column, String input ) {
-        SqlDB dataBase = new SqlDB();
+        QueststoreDao dao = new Queststore();
         String table = "Mentor";
         String sqlQuery = "UPDATE"+ table +" SET " + column +"='"+input+"' WHERE id ="+mentorsId+";";
-        dataBase.updateData(table, sqlQuery);
+        dao.updateDataInTable(table, sqlQuery);
 
     }
 
