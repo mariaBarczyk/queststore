8410265ca7de2373823194c4652cb972e82ef105
Justyna Fornalik <jfornalik@gmail.com>
2018-01-23 22:11:31 +0100

    Add GroupDB class

---
+++ src/dao/GroupDB.java
@@ -0,0 +1,10 @@
+public class GroupDB{
+    
+    public void addNewGroup(String groupName){
+        SqlDB dataBase = new SqlDB();
+        String table = "Group";
+        String columns = "(name)";
+        String values = "'"+groupName+"'";
+        dataBase.insertDataIntoTable(table, column, values);
+    }
+}
