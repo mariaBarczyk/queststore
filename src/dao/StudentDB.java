acc6bf92e46300fa81d8bb94dc5e008355c1a4db
Justyna Fornalik <jfornalik@gmail.com>
2018-01-23 23:31:42 +0100

    Add insertNewStudent method

--- src/dao/StudentDB.java
+++ src/dao/StudentDB.java
@@ -4,5 +4,12 @@ import java.sql.*;
 
 public class StudentDB{
 
-    
+    public void insertNewStudent(String studentName, String studentLastName, String studentEmail, String studentPassword, int id_group, int id_wallet) {
+        QueststoreDao dao = new Queststore();
+        String table = "Student";
+        int idStatus = 3;
+        String columns = "(first_name, last_name, email, password, id_status, id_group, id_wallet)";
+        String values = "'" + studentName + "', '" + studentLastName + "', '" + studentEmail + "','" + studentPassword + "', " + idStatus + ", " + id_group + ", " + id_wallet"";
+        dao.insertDataIntoTable(table, columns, values);
+    }
 }
