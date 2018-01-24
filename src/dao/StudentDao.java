db3c6d396b3a1ea92f8835b0c59e6aa10474a102
Justyna Fornalik <jfornalik@gmail.com>
2018-01-24 13:20:51 +0100

    Add insertNewWallet method

---
+++ src/dao/StudentDao.java
@@ -0,0 +1,25 @@
+package dao;
+
+import java.sql.*;
+
+public class StudentDao{
+
+    public void insertNewWallet(){
+        QueststoreDao dao = new QueststoreDao();
+        String tableName = "Wallet";
+        String columns = "(totalCoolcoins, balance, id_artifact)";
+        String values = (0+", "+0+", "+null);
+        dao.insertDataIntoTable(tableName, columns, values);
+
+    }
+
+    public void insertNewStudent(String studentName, String studentLastName, String studentEmail, String studentPassword, int id_group) {
+        QueststoreDao dao = new QueststoreDao();
+        insertNewWallet();
+        String table = "Student";
+        int idStatus = 3;
+        String columns = "(first_name, last_name, email, password, id_status, id_group, id_wallet)";
+        String values = "'" + studentName + "', '" + studentLastName + "', '" + studentEmail + "','" + studentPassword + "', " + idStatus + ", " + id_group + ", " + id_wallet + "";
+        dao.insertDataIntoTable(table, columns, values);
+    }
+}
