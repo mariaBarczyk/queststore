8975ab976124a61dc201a021485d964e9995f2fa
Justyna Fornalik <jfornalik@gmail.com>
2018-01-24 17:58:02 +0100

    Correct addNewGroup method

--- src/dao/GroupDao.java
+++ src/dao/GroupDao.java
@@ -3,10 +3,10 @@ package dao;
 public class GroupDao{
 
     public void addNewGroup(String groupName){
-        QuestStoreDao dao = new QueststoreDao();
+        QueststoreDao dao = new QueststoreDao();
         String table = "Group";
         String columns = "(name)";
         String values = "'"+groupName+"'";
-        dao.insertDataIntoTable(table, column, values);
+        dao.insertDataIntoTable(table, columns, values);
     }
 }
