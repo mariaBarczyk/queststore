0079e8cb0e379f0f1845b9e969f6385652d05bc4
Justyna Fornalik <jfornalik@gmail.com>
2018-01-25 12:01:40 +0100

    Close connection after inserting data

--- src/dao/QueststoreDao.java
+++ src/dao/QueststoreDao.java
@@ -56,6 +56,8 @@ public class QueststoreDao {
             statement.executeUpdate(sql);
         } catch (Exception e) {
             e.printStackTrace();
+        } finally {
+        closeConnection();
         }
     }
 
