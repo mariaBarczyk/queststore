05a7ce260e117f7120cbdecc81a06aefe042e307
mariaBarczyk <barczyk.maria92@gmail.com>
2018-01-29 15:53:55 +0100

    Comment out insertNewStudent method in StudentDao

--- src/dao/StudentDao.java
+++ src/dao/StudentDao.java
@@ -14,14 +14,14 @@ public class StudentDao{
 
     }
 
-    public void insertNewStudent(String studentName, String studentLastName, String studentEmail, String studentPassword, int id_group) {
-        QueststoreDao dao = new QueststoreDao();
-        insertNewWallet();
-        int idWallet = dao.getMaxValueInTable("Wallet", "id_wallet");
-        String table = "Student";
-        int idStatus = 3;
-        String columns = "(first_name, last_name, email, password, id_status, id_group, id_wallet)";
-        String values = "'" + studentName + "', '" + studentLastName + "', '" + studentEmail + "','" + studentPassword + "', " + idStatus + ", " + id_group + ", " + idWallet + "";
-        dao.insertDataIntoTable(table, columns, values);
-    }
+//    public void insertNewStudent(String studentName, String studentLastName, String studentEmail, String studentPassword, int id_group) {
+//        QueststoreDao dao = new QueststoreDao();
+//        insertNewWallet();
+//        int idWallet = dao.getMaxValueInTable("Wallet", "id_wallet");
+//        String table = "Student";
+//        int idStatus = 3;
+//        String columns = "(first_name, last_name, email, password, id_status, id_group, id_wallet)";
+//        String values = "'" + studentName + "', '" + studentLastName + "', '" + studentEmail + "','" + studentPassword + "', " + idStatus + ", " + id_group + ", " + idWallet + "";
+//        dao.insertDataIntoTable(table, columns, values);
+//    }
 }
