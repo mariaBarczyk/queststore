14fd00e9f184fc8ffc3be33817dbb8a6e8d6d83f
mariaBarczyk <barczyk.maria92@gmail.com>
2018-01-31 10:57:21 +0100

    Fix bug with sql querry in GroupDao

--- src/dao/GroupDao.java
+++ src/dao/GroupDao.java
@@ -4,8 +4,8 @@ public class GroupDao{
 
     public void addNewGroup(String groupName){
         QueststoreDao dao = new QueststoreDao();
-        String table = "Group";
-        String columns = "(name)";
+        String table = "Groups";
+        String columns = "'name'";
         String values = "'"+groupName+"'";
         dao.insertDataIntoTable(table, columns, values);
     }
