aa9ba0198e257e7e8f3473ca41b44424cee478c0
mariaBarczyk <barczyk.maria92@gmail.com>
2018-01-31 21:11:36 +0100

    Create getAllMentorsCollection in MentorDao

--- src/dao/MentorDao.java
+++ src/dao/MentorDao.java
@@ -2,14 +2,19 @@ package dao;
 
 
 import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+import java.util.ArrayList;
+
+import model.MentorModel;
 
 public class MentorDao extends QueststoreDao{
 
     public void insertNewMentor(String mentorName, String mentorLastName, String mentorEmail, String mentorPassword) {
         String loginTable = "Login";
         int idStatus = 2;
-        String loginColumns = "(email, password, id_status)";
-        String loginValues = "('"+mentorEmail+"', '"+ mentorPassword + "', " + idStatus + ");";
+        String loginColumns = "'email', 'password', 'id_status'";
+        String loginValues = "'"+mentorEmail+"', '"+ mentorPassword + "', " + idStatus;
         insertDataIntoTable(loginTable, loginColumns, loginValues);
 
         String condition = "email = '" + mentorEmail + "' AND password = '"+mentorPassword+"'";
@@ -22,18 +27,41 @@ public class MentorDao extends QueststoreDao{
 
         int id_group = 1;
         String table = "Mentor";
-        String columns = "(first_name, last_name, id_login, id_status, id_group)";
-        String values = "('" + mentorName + "', '" + mentorLastName + "', " + idLogin +", "+ idStatus + ", " + id_group + ");";
+        String columns = "'first_name', 'last_name', 'id_login', 'id_status', id_group";
+        String values = "'" + mentorName + "', '" + mentorLastName + "', " + idLogin +", "+ idStatus + ", " + id_group;
         insertDataIntoTable(table, columns, values);
     }
 
-    /*public void updateMentorData(int mentorsId, String column, String input ) {
+    public void updateMentorData(int mentorsId, String column, String newData ) {
         String table = "Mentor";
-        String setQuery = column +"='"+input+"'";
+        String setQuery = column +"='"+ newData +"'";
         String condition = ("id_mentor = " + mentorsId);
-        //updateDataInTable(table, setQuery, condition);
+        QueststoreDao dao = new QueststoreDao();
+        dao.updateDataInTable(table, setQuery, condition);
 
-    }*/
+    }
+    public List<MentorModel> getAllMentorsCollection() {
+
+        List<MentorModel> mentorCollection = new ArrayList<>();
+        QueststoreDao dao = new QueststoreDao();
+        String columns = "email, password, Mentor.first_name, Mentor.last_name, Mentor.id_mentor";
+        String joinStatement = "Mentor.id_login = Login.id_login";
+        ResultSet result = dao.selectFromJoinedTables(columns, "Login", "Mentor", joinStatement);
+        try {
+            while (result.next()) {
+                String firstName = result.getString("first_name");
+                String lastName = result.getString("last_name");
+                String email = result.getString("email");
+                String password = result.getString("password");
+                int id = result.getInt("id_mentor");
+                MentorModel mentor = new MentorModel(id, firstName, lastName, email, password);
+                mentorCollection.add(mentor);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return mentorCollection;
+    }
 
 }
 
