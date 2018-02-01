ad23abf4defbe9f1814e9363cc6ed2bea040f945
mariaBarczyk <barczyk.maria92@gmail.com>
2018-02-01 11:40:34 +0100

    Create inserting new item to database and finding type_id method in ItemDao

--- src/dao/ItemDao.java
+++ src/dao/ItemDao.java
@@ -1,4 +1,33 @@
 package dao;
 
+import model.ItemModel;
+import model.QuestModel;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 public class ItemDao {
+
+    public void insertNewItem(ItemModel item) {
+        String table = "Item";
+        String columns = "'name', 'description', 'value', 'id_type'";
+        String values = "'"+ item.getName() + "','"+ item.getDescription()+"',"+item.getValue()+", "+findIdType(item.getType());
+        QueststoreDao dao = new QueststoreDao();
+        dao.insertDataIntoTable(table, columns, values);
+    }
+
+    public int findIdType(String typeName) {
+        QueststoreDao dao = new QueststoreDao();
+        ResultSet result = dao.selectDataFromTable("ItemType", "id_type", "name='"+typeName+"'");
+        int idType = 0;
+        try {
+            idType = result.getInt("id_type");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            dao.closeConnection();
+        }
+        return idType;
+
+    }
 }
