8998cc47237131c1c6fee0b1d6bed198f1f5d414
mariaBarczyk <barczyk.maria92@gmail.com>
2018-02-02 13:06:45 +0100

    Add constructor in TranstactionDao

--- src/dao/TransactionDao.java
+++ src/dao/TransactionDao.java
@@ -1,5 +1,10 @@
 package dao;
 
-public class TransactionDao {
-    
+public class TransactionDao extends QueststoreDao {
+
+    public void insertTransaction(int idStudent, int idItem) {
+        String values =  ""+ idStudent +", " + idItem+ "," + 0 + "";
+        insertDataIntoTable("Transactions", "id_student, id_item, used", values);
+
+    }
 }
