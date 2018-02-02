1086e8247198f1c969ebdfdb736439b96ac25429
mariaBarczyk <barczyk.maria92@gmail.com>
2018-02-02 12:57:24 +0100

    Create getStudentWallet and selecting student method in StudentDao

--- src/dao/StudentDao.java
+++ src/dao/StudentDao.java
@@ -1,5 +1,8 @@
 package dao;
 
+import model.StudentModel;
+import model.WalletModel;
+
 import java.sql.*;
 
 public class StudentDao extends QueststoreDao{
@@ -53,8 +56,44 @@ public class StudentDao extends QueststoreDao{
         String columns = "(first_name, last_name, id_login, id_status, id_group)";
         String values = "('" + studentName + "', '" + studentLastName + "', " + idLogin +", "+ idStatus + ", " + id_group + ");";
         insertDataIntoTable(table, columns, values);
-
         insertNewWallet(idLogin);
     }
 
+    private WalletModel getStudentWallet(int idWallet) {
+        ResultSet result = selectDataFromTable("Wallet","totalCoolcoins, balance", "id_wallet='"+idWallet+"'");
+        WalletModel wallet = null;
+        try {
+            while (result.next()) {
+                int totalCoins = result.getInt("totalCoolcoins");
+                int balance = result.getInt("balance");
+                wallet = new WalletModel(idWallet, totalCoins, balance);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return wallet;
+    }
+
+    public StudentModel selectStudent(int idLogin) {
+        String columns = "Login.email, Login.password, id_student, first_name, last_name, id_wallet";
+        String joinStatement = "Login.id_login=Student.id_login";
+        // CREATE NEW SELET JOIN WITH WHERE (idLogin ==)
+        ResultSet result = selectFromJoinedTables(columns, "Student", "Login", joinStatement);
+        StudentModel student = null;
+        try {
+            while (result.next()) {
+                String email = result.getString("email");
+                String password = result.getString("password");
+                int id = result.getInt("id_student");
+                String firstName = result.getString("first_name");
+                String lastName = result.getString("last_name");
+                int idWallet = result.getInt("id_wallet");
+                WalletModel wallet = getStudentWallet(idWallet);
+                student = new StudentModel(id, firstName, lastName, email, password, wallet);
+            }
+        }catch (SQLException e) {
+                e.printStackTrace();
+        }
+        return student;
+    }
 }
