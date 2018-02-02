012acb8af240aee42b0b1a88838bbbfdea2c2344
mariaBarczyk <barczyk.maria92@gmail.com>
2018-02-02 13:08:25 +0100

    Create methods to get collection of items

--- src/dao/ItemDao.java
+++ src/dao/ItemDao.java
@@ -1,12 +1,15 @@
 package dao;
 
+import model.ArtifactModel;
 import model.ItemModel;
 import model.QuestModel;
 
 import java.sql.ResultSet;
+import java.util.List;
+import java.util.ArrayList;
 import java.sql.SQLException;
 
-public class ItemDao {
+public class ItemDao extends QueststoreDao {
 
     public void insertNewItem(ItemModel item) {
         String table = "Item";
@@ -28,6 +31,35 @@ public class ItemDao {
             dao.closeConnection();
         }
         return idType;
+    }
 
+    private ItemModel createItemObject(int idItem, String typeName, String itemName, String description, int price) {
+
+        if (typeName.equals("Quest")) {
+            return new QuestModel(idItem, typeName, itemName, description, price);
+        } else {
+            return new ArtifactModel(idItem, typeName, itemName, description, price);
+        }
+    }
+
+    public List<ItemModel> getItemCollectionByType(String typeName) {
+        List<ItemModel> itemCollection = new ArrayList<>();
+        int idType = findIdType(typeName);
+        String columns = "id_item, item_name, description, price";
+        String condition  = "id_type='" + idType +"'";
+        ResultSet result = selectDataFromTable("Item", columns, condition);
+        try {
+            while (result.next()) {
+                int idItem = result.getInt("id_item");
+                String name = result.getString("item_name");
+                String description = result.getString("description");
+                int price = result.getInt("price");
+                ItemModel item = createItemObject(idItem, typeName, name, description, price);
+                itemCollection.add(item);
+                }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return itemCollection;
     }
 }
