4d966a1dda61ee7aff1ee4faf497aaabfaaae5d8
mariaBarczyk <barczyk.maria92@gmail.com>
2018-02-02 14:37:38 +0100

    Reslove problem after merge in TransactionDao

--- src/dao/TransactionDao.java
+++ src/dao/TransactionDao.java
@@ -3,8 +3,8 @@ package dao;
 public class TransactionDao extends QueststoreDao {
 
     public void insertTransaction(int idStudent, int idItem) {
-        String values =  ""+ idStudent +", " + idItem+ "," + 0 + "";
-        insertDataIntoTable("Transactions", "id_student, id_item, used", values);
+        String values =  "("+ idStudent +", " + idItem+ "," + 0 + ")";
+        insertDataIntoTable("Transactions ", "(id_student, id_item, used)", values);
 
     }
 }
