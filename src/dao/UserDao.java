50c4aaf179723465ded081663405c39a81e226a9
mariaBarczyk <barczyk.maria92@gmail.com>
2018-02-06 20:40:29 +0100

    Remove methods using to log in from UserDao

--- src/dao/UserDao.java
+++ src/dao/UserDao.java
@@ -5,7 +5,7 @@ import java.sql.*;
 public class UserDao {
 
     private DatabaseConnection database = DatabaseConnection.getInstance();
-    public Connection connection;
+    private Connection connection;
     private Statement statement;
 
     public UserDao(){
@@ -16,104 +16,120 @@ public class UserDao {
             e.printStackTrace();
         }
     }
-
-    public ResultSet selectDataFromTable(String tableName, String columns, String condition) {
+    private ResultSet executeQuery(String sql) {
         ResultSet result = null;
         try {
-            String sql = "SELECT " + columns + " FROM " + tableName + " WHERE " + condition + ";";
-            System.out.println(sql);
             result = statement.executeQuery(sql);
-        } catch (Exception e) {
+        } catch (SQLException e) {
             System.err.println(e.getClass().getName() + ": " + e.getMessage());
         }
         return result;
+    }
+
+    private void executeUpdate(String sql) {
+        try {
+            statement.executeUpdate(sql);
+        } catch (Exception e) {
+            System.err.println(e.getClass().getName() + ": " + e.getMessage());
+        }
+    }
+
+    public int getIntFromResult(ResultSet result, String columnName) {
+        int intData = 0;
+        try {
+            intData = result.getInt(columnName);
+        } catch (SQLException e) {
+            System.err.println(e.getClass().getName() + ": " + e.getMessage());
+        }
+        return intData;
+    }
+
+    public String getStringFromResult(ResultSet result, String columnName) {
+        String stringData = null;
+        try {
+            stringData = result.getString(columnName);
+        } catch (SQLException e) {
+            System.err.println(e.getClass().getName() + ": " + e.getMessage());
+        }
+        return stringData;
+    }
+
+    public ResultSet selectDataFromTable(String tableName, String columns, String condition) {
+        String sql = "SELECT " + columns + " FROM " + tableName + " WHERE " + condition + ";";
+        return executeQuery(sql);
+
     }
 
     public ResultSet selectFromJoinedTables(String columns, String tableName, String joinTable, String joinStatement) {
-        ResultSet result = null;
-        try {
-            String sql = "SELECT " + columns + " FROM " + tableName + " JOIN " + joinTable + " ON " + joinStatement + ";";
-            System.out.println(sql);
-            result = statement.executeQuery(sql);
-        } catch (Exception e) {
-            System.err.println(e.getClass().getName() + ": " + e.getMessage());
-        }
-        return result;
+        String sql = "SELECT " + columns + " FROM " + tableName + " JOIN " + joinTable + " ON " + joinStatement + ";";
+        return  executeQuery(sql);
+
     }
     
-    public void insertDataIntoTable(String tableName, String columns, String values)  {
-        try {
-            String sql = "INSERT INTO " + tableName + columns + " VALUES " + values;
-            System.out.println(sql);
-            statement.executeUpdate(sql);
-        } catch (Exception e) {
-            System.err.println(e.getClass().getName() + ": " + e.getMessage());
-        }
+    public void insertDataIntoTable(String tableName, String columns, String values) {
+        String sql = "INSERT INTO " + tableName + columns + " VALUES " + values;
+        executeUpdate(sql);
     }
 
     public void updateDataInTable(String table, String setQuerry, String condition) {
-        try {
-            String sql = "UPDATE " + table + " SET " + setQuerry + " WHERE " + condition;
-            System.out.println(sql);
-            statement.executeUpdate(sql);
-        } catch (Exception e) {
-            System.err.println(e.getClass().getName() + ": " + e.getMessage());
-        }
+        String sql = "UPDATE " + table + " SET " + setQuerry + " WHERE " + condition;
+        executeUpdate(sql);
     }
 
-    public int findStatusId(String login, String password) {
-        int idStatus = 0;
-        try {
-            ResultSet result = selectDataFromTable("Login", "id_status", "email='" + login + "' AND password='" + password + "'");
-            while (result.next()) return result.getInt("id_status");
-            System.out.println(idStatus);
-        } catch (SQLException e) {
-            System.err.println(e.getClass().getName() + ": " + e.getMessage());
-        }
-        return idStatus;
-    }
-
-    public String findStatus(int idStatus) {
-        String statusName = null;
-        try {
-            ResultSet result = selectDataFromTable("status", "name", "id_status=" + idStatus);
-            statusName = result.getString("name");
-        } catch (Exception e) {
-            System.err.println(e.getClass().getName() + ": " + e.getMessage());
-        }
-        return statusName;
-    }
-
-    public int findStatusIdByName(String name) {
-        int idStatus = 0;
-        try {
-            ResultSet result = selectDataFromTable("status", "id_status", "name='" + name + "'");
-            while (result.next()) return result.getInt("id_status");
-            System.out.println(idStatus);
-        } catch (SQLException e) {
-            System.err.println(e.getClass().getName() + ": " + e.getMessage());
-        }
-        return idStatus;
-    }
-
-    public int findLoginId(String login, String password) {
-        int loginId = 0;
-
-        try {
-            ResultSet result = selectDataFromTable("Login", "id_login", " email='" + login + "' AND password='" + password + "'");
-            loginId = result.getInt("id_login");
-        } catch (Exception e) {
-            System.err.println(e.getClass().getName() + ": " + e.getMessage());
-        }
-        return loginId;
-    }
-
-    public void insertNewLogin(String email, String password, int idStatus){
-        String loginTable = "Login";
-        String loginColumns = "(email, password, id_status)";
-        String loginValues = "('"+email+"', '"+ password + "', " + idStatus + ");";
-        insertDataIntoTable(loginTable, loginColumns, loginValues);
-
-    }
+//    public int findStatusId(String login, String password) {
+//        int idStatus = 0;
+//        try {
+//            ResultSet result = selectDataFromTable("Login", "id_status", "email='" + login + "' AND password='" + password + "'");
+//            while (result.next())
+//                idStatus = result.getInt("id_status");
+//        } catch (SQLException e) {
+//            System.err.println(e.getClass().getName() + ": " + e.getMessage());
+//        }
+//        return idStatus;
+//    }
+//
+//    public String findStatus(int idStatus) {
+//        String statusName = null;
+//        try {
+//            ResultSet result = selectDataFromTable("status", "name", "id_status=" + idStatus);
+//            statusName = result.getString("name");
+//        } catch (Exception e) {
+//            System.err.println(e.getClass().getName() + ": " + e.getMessage());
+//        }
+//        return statusName;
+//    }
+//
+//    public int findStatusIdByName(String name) {
+//        int idStatus = 0;
+//        try {
+//            ResultSet result = selectDataFromTable("status", "id_status", "name='" + name + "'");
+//            while (result.next())
+//                idStatus =  result.getInt("id_status");
+//            System.out.println(idStatus);
+//        } catch (SQLException e) {
+//            System.err.println(e.getClass().getName() + ": " + e.getMessage());
+//        }
+//        return idStatus;
+//    }
+//
+//    public int findLoginId(String login, String password) {
+//        int loginId = 0;
+//
+//        try {
+//            ResultSet result = selectDataFromTable("Login", "id_login", " email='" + login + "' AND password='" + password + "'");
+//            loginId = result.getInt("id_login");
+//        } catch (Exception e) {
+//            System.err.println(e.getClass().getName() + ": " + e.getMessage());
+//        }
+//        return loginId;
+//    }
+//
+//    public void insertNewLogin(String email, String password, int idStatus){
+//        String loginTable = "Login";
+//        String loginColumns = "(email, password, id_status)";
+//        String loginValues = "('"+email+"', '"+ password + "', " + idStatus + ");";
+//        insertDataIntoTable(loginTable, loginColumns, loginValues);
+//
+//    }
 }
 
