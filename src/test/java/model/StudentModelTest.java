99e2a2533c357054f9dcc56081adfeacb1e96965
Adam Kruk <adam.kruk91@gmail.com>
2018-03-22 09:50:24 +0100

    Add test to check if wallet is added

--- src/test/java/model/StudentModelTest.java
+++ src/test/java/model/StudentModelTest.java
@@ -1,5 +1,19 @@
+package model;
+
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class StudentModelTest {
 
+    private WalletModel walletModel = Mockito.mock(WalletModel.class);
+
+    @Test
+    public void checkIfStudentModelHasWallet(){
+        StudentModel studentModel = new StudentModel(1, "Adam", "Kruk", "adam@gmal.com",
+                                                "lala", "2016.2", walletModel);
+        assertEquals(walletModel, studentModel.getMyWallet());
+    }
 }
