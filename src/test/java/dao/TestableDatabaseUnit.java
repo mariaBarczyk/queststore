74d56865b8bcd5c2830ad0b5d5886c11933eeed7
Michal Gruszka <m.gruszka215@gmail.com>
2018-03-22 17:09:57 +0100

    Add newlines to improve code readability

--- src/test/java/dao/TestableDatabaseUnit.java
+++ src/test/java/dao/TestableDatabaseUnit.java
@@ -9,15 +9,18 @@ import static org.junit.jupiter.api.Assertions.*;
 class TestableDatabaseUnit {
 
     private ManipulationDao dao;
+
     @BeforeAll
     static void beforeAll() throws SQLException{
         TestDatabase.prepareTestInstance();
     }
+
     @BeforeEach
     void beforeEach() {
         dao = new ManipulationDao();
         dao.executeUpdate("BEGIN TRANSACTION;");
     }
+
     @AfterEach
     void afterEach() {
         dao.executeUpdate("ROLLBACK;");
