967b6d4f501a7d6e890da6af2d80c461429f87c3
mrezlik <mrezlik@gmail.com>
2018-03-22 18:04:25 +0100

    Add test student artifact

--- src/test/java/dao/TransactionDaoTest.java
+++ src/test/java/dao/TransactionDaoTest.java
@@ -1,35 +1,41 @@
 package dao;
 
 import model.ItemModel;
-import model.StudentModel;
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
+
 
 class TransactionDaoTest extends TestableDatabaseUnit{
 
     @Test
     void testUpdateStatusOfTransaction() {
-        ItemModel mockItem =  Mockito.mock(ItemModel.class);
+        ItemModel mockItem = Mockito.mock(ItemModel.class);
         Mockito.when(mockItem.getID()).thenReturn(1);
-        TransactionDao td = new TransactionDao();
-        td.updateStatusOfTransaction(mockItem);
+        transactionDao.updateStatusOfTransaction(mockItem);
 
         String query = "SELECT count(*) as rows_count FROM Transactions WHERE id_item = 1;";
-        ResultSet rs = td.executeSelect(query);
+        ResultSet rs = transactionDao.executeSelect(query);
         try {
             int i = rs.getInt("rows_count");
             assertEquals(4, i);
         } catch (SQLException e) {
             e.printStackTrace();
         }
+    }
 
+    @Test
+    void testGetStudentArtifact() {
+        List<ItemModel> itemModel = transactionDao.getStudentArtifact(2);
 
-
+        assertEquals(itemModel.get(0).getID(), 5);
+        assertEquals(itemModel.get(1).getID(), 3);
 
     }
 
